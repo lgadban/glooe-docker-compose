5b25882643c49846be8ff2327f6199b76e434cda
Lawrence Gadban <lawrence.gadban@solo.io>
2021-06-22 09:47:03 -0500

    remove control plane

--- prepare-directories.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p ./data/artifact/artifacts/gloo-system
-mkdir -p ./data/config/{proxies,upstreams,upstreamgroups,routetables,authconfigs}/gloo-system
-mkdir -p ./data/secret/secrets/{default,gloo-system}
-
